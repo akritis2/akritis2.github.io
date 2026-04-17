96850831d44b0a4773e6ef57420b7014b3968ab3
Akriti Singh <akritis2@uci.edu>
2026-04-16 20:51:39 -0700

    commented up event pages

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 <a href="contact-us.html" class="contact-text-link">Contact Us</a>
                 <a href="https://discord.gg/QSYSCFb" target="_blank" class="glow-link">Discord</a>
                 <a href="https://www.instagram.com/codec.uci/" target="_blank" class="glow-link">Instagram</a>
+                <a href="https://www.linkedin.com/in/codec-uci" target="_blank" class="glow-link">LinkedIn</a>
             </div>
         </div>
         <p class="copyright">© 2026 CODEC | Community of Diverse Empowerment in Computing</p>
